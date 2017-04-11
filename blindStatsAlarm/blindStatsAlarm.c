ae0086b0e3dd5da13cfe17df64c0b2f021edfc5f
peter <peter.niederer@sonnenkinder.org>
2017-04-11 19:01:34 +0200

    init

--- blindStatsAlarm/blindStatsAlarm.c
+++ blindStatsAlarm/blindStatsAlarm.c
@@ -21,13 +21,13 @@ int8_t alarmOn;
 
 void initBuzzerTimer()
 {
-	TCCR0A = (1 << COM0B1) | (1 << WGM00);
+	TCCR0A = (1 << COM0B1) | (1 << WGM00) | (1<< WGM01);
 	TCCR0B = (1 << WGM02);
 	OCR0A =  200;
 	OCR0B =  100;    // should together give a squarewave of 5k
 	TCNT0 = 0;
-	PORTB &= ~(1 << PB0);   // initial value 0
-	DDRB |= (1 << DDB0);    // set as ouput
+	PORTB &= ~(1 << PB1);   // initial value 0
+	DDRB |= (1 << DDB1);    // set as ouput
 }
 
 void startBuzzerTimer()
@@ -47,8 +47,10 @@ void toggleBuzzerAlarm()
 	if (alarmOn == 0) {
 		initBuzzerTimer();
 		startBuzzerTimer();
+		alarmOn = 1;
 	} else {
 		stopBuzzerTimer();
+		alarmOn = 0;
 	}
 	
 }
@@ -60,9 +62,9 @@ ISR(TIMER1_COMPA_vect)
 	if (tickCnt > 1) {
 		tickCnt = 0;
 		++ secsCnt;
-	}
-	if (secsCnt > alarmSecs)  {
-		toggleBuzzerAlarm();
+		if (secsCnt > alarmSecs)  {
+			toggleBuzzerAlarm();
+		}
 	}
 }
 
